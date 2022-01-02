53cd02afc4dbf12424c0101d140121ef2dc98e3e
notexiqe <94329098+notexiqe@users.noreply.github.com>
2022-01-03 04:56:54 +0700

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,16 @@
 //const WCHAR* FILE_NAME = L"C:\\Program Files\\HxD\\HxD.exe";
 const WCHAR* FILE_NAME = L"C:\\Windows\\System32\\notepad.exe";
 
+#define RvaToVa(Base, Offset) ((PVOID)((ULONG64)Base + (ULONG)Offset))
+
 int main() {
     HANDLE hFile = NULL;
     HANDLE hFileMap = NULL;
     LPVOID pMapImage = NULL;
-    WORD check_MZ;
+    PIMAGE_DATA_DIRECTORY pDataDirectory = NULL;
+    WORD check_MZ = 0;
+    DWORD* NumberOfRvaAndSize = NULL;
+    LONG e_lfanew;
 
     hFile = CreateFile(FILE_NAME, FILE_READ_DATA, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_READONLY, NULL);
     if (hFile == INVALID_HANDLE_VALUE) {
@@ -23,7 +28,7 @@ int main() {
     SetFilePointer(hFile, 0, 0, FILE_BEGIN);
     if (ReadFile(hFile, &check_MZ, 0x02, NULL, NULL)) {
         if (check_MZ == IMAGE_DOS_SIGNATURE) {
-            fprintf(stdout, "\te_magic: \"MZ\"\n");
+            fprintf(stdout, "\te_magic:  MZ\n");
         }
         else {
             fprintf(stderr, "\tError: File format is not PE\n");
@@ -50,37 +55,39 @@ int main() {
         CloseHandle(hFile);
         return (-5);
     }
-    
+
     // _IMAGE_DOS_HEADER
     if (*(BYTE*)((ULONG64)pMapImage + 0x18) < 0x40) {
         fprintf(stderr, "\tError: e_lfarlc less then 40h\n");
-        return (-6);
         UnmapViewOfFile(pMapImage);
         CloseHandle(hFileMap);
         CloseHandle(hFile);
+        return (-6);
     }
     else {
-        fprintf(stdout, "\te_lfarlc: %x\n", *(BYTE*)((ULONG64)pMapImage + 0x18));
+        fprintf(stdout, "\te_lfarlc: %X\n", *(BYTE*)((ULONG64)pMapImage + 0x18));
     }
 
-    fprintf(stdout, "\te_lfanew: %x\n", *(LONG*)((ULONG64)pMapImage + 0x3C));
+    e_lfanew = *(LONG*)((ULONG64)pMapImage + 0x3C);
+    fprintf(stdout, "\te_lfanew: %X\n", e_lfanew);
 
     //PIMAGE_NT_HEADERS64
     fprintf(stdout, "IMAGE_NT_HEADER\n");
-    if (*(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C)) == IMAGE_NT_SIGNATURE) {
-        fprintf(stdout, "\tSignature: \"%s\"\n", (char*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C)));
-    }
-    else {
+    if (*(DWORD*)(((ULONG64)pMapImage) + e_lfanew) != IMAGE_NT_SIGNATURE) {
         fprintf(stderr, "\tSignature: \"PE\" not found\n");
         UnmapViewOfFile(pMapImage);
         CloseHandle(hFileMap);
         CloseHandle(hFile);
-        return (-6);
+        return (-7);
+    }
+    else {
+        fprintf(stdout, "\tSignature: \"%s\"\n", (char*)(((ULONG64)pMapImage) + e_lfanew));
+        
     }
 
     fprintf(stdout, "\n\tIMAGE_FILE_HEADER\n");
 
-    switch (*(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3c) + 0x04)) {
+    switch (*(WORD*)(((ULONG64)pMapImage) + e_lfanew + 0x04)) {
     default: {
         fprintf(stdout, "\t\tMachine: unknown or not added;\n");
         break;
@@ -99,71 +106,55 @@ int main() {
     }
     }
 
-    if (*(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x06) < 0x60) {
-        fprintf(stdout, "\t\tNumberOfSections: %04x\n", *(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x06));
-    }
-    else {
+    if (*(WORD*)(((ULONG64)pMapImage) + e_lfanew + 0x06) >= 0x60) {
         fprintf(stderr, "\t\tNumberOfSections: incorrect value\n");
         return(-7);
     }
-
-    fprintf(stdout, "\t\tTimeDateStamp: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x08));
-    fprintf(stdout, "\t\tPointerToSymbolTable: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0xC));
-    fprintf(stdout, "\t\tNumberOfSymbols: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x10));
-    fprintf(stdout, "\t\tSizeOfOptionalHeader: %04x\n", *(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x14));
-    fprintf(stdout, "\t\tCharacteristics: %04x\n", *(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x16));
-    fprintf(stdout, "\n\tIMAGE_OPTIONAL_HEADER\n");
-    if (*(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x18) == IMAGE_NT_OPTIONAL_HDR64_MAGIC) {
-        fprintf(stdout, "\t\tMagic: x64\n");
-    }
     else {
+        fprintf(stdout, "\t\tNumberOfSections: %04X\n", *(WORD*)(((ULONG64)pMapImage) + e_lfanew + 0x06));
+    }
+
+    fprintf(stdout, "\t\tSizeOfOptionalHeader: %04X\n", *(WORD*)(((ULONG64)pMapImage) + e_lfanew + 0x14));
+    fprintf(stdout, "\t\tCharacteristics: %04X\n", *(WORD*)(((ULONG64)pMapImage) + e_lfanew + 0x16));
+    fprintf(stdout, "\n\tIMAGE_OPTIONAL_HEADER\n");
+    if (*(WORD*)(((ULONG64)pMapImage) + e_lfanew + 0x18) != IMAGE_NT_OPTIONAL_HDR64_MAGIC) {
         fprintf(stderr, "\t\tMagic: Application is not 64-bit;\n");
         UnmapViewOfFile(pMapImage);
         CloseHandle(hFileMap);
         CloseHandle(hFile);
         return (-8);
     }
-
-    fprintf(stdout, "\t\tMajorLinkerVersion: %02x\n", *(BYTE*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x1A));
-    fprintf(stdout, "\t\tMinorLinkerVersion: %02x\n", *(BYTE*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x1B));
-    fprintf(stdout, "\t\tSizeOfCode: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x1C));
-    fprintf(stdout, "\t\tSizeOfInitializedData: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x20));
-    fprintf(stdout, "\t\tSizeOfUninitializedData: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x24));
-
-    if (*(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x28) > 0) {
-        fprintf(stdout, "\t\tAddressOfEntryPoint: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x28));
-    }
     else {
+        fprintf(stdout, "\t\tMagic: x64\n");
+    }
+
+    if (*(DWORD*)(((ULONG64)pMapImage) + e_lfanew + 0x28) <= 0) {
         fprintf(stderr, "\t\tAddressOfEntryPoint: incorrect value\n");
         UnmapViewOfFile(pMapImage);
         CloseHandle(hFileMap);
         CloseHandle(hFile);
         return (-9);
     }
+    else {
+        fprintf(stdout, "\t\tAddressOfEntryPoint: %08X\n", *(DWORD*)(((ULONG64)pMapImage) + e_lfanew + 0x28));
+    }
 
-    fprintf(stdout, "\t\tBaseOfCode: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x2C));
-    fprintf(stdout, "\t\tImageBase: %016llx\n", *(ULONGLONG*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x30));
-    fprintf(stdout, "\t\tSectionAligment: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x38));
-    fprintf(stdout, "\t\tFileAligment: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x3C));
-    fprintf(stdout, "\t\tMajorOperatingSystemVersion: %04x\n", *(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x40));
-    fprintf(stdout, "\t\tMinorOperatingSystemVersion: %04x\n", *(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x42));
-    fprintf(stdout, "\t\tMajorImageVersion: %04x\n", *(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x44));
-    fprintf(stdout, "\t\tMinorImageVersion: %04x\n", *(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x46));
-    fprintf(stdout, "\t\tMajorSybsystemVersion: %04x\n", *(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x48));
-    fprintf(stdout, "\t\tMinorSybsystemVersion: %04x\n", *(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x4A));
-    fprintf(stdout, "\t\tWin32VersionValue: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x4C));
-    fprintf(stdout, "\t\tSizeOfImage: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x50));
-    fprintf(stdout, "\t\tSizeOfHeaders: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x54));
-    fprintf(stdout, "\t\tCheckSum: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x58));
-    fprintf(stdout, "\t\tSubsystem: %04x\n", *(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x5C));
-    fprintf(stdout, "\t\tDllCharacteristics: %04x\n", *(WORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x5E));
-    fprintf(stdout, "\t\tSizeOfStackReserve: %016llx\n", *(ULONGLONG*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x60));
-    fprintf(stdout, "\t\tSizeOfStackCommit: %016llx\n", *(ULONGLONG*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x68));
-    fprintf(stdout, "\t\tSizeOfHeapReserve: %016llx\n", *(ULONGLONG*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x70));
-    fprintf(stdout, "\t\tSizeOfHeapCommit: %016llx\n", *(ULONGLONG*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x78));
-    fprintf(stdout, "\t\tLoaderFlags: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x80));
-    fprintf(stdout, "\t\tNumberOfRvaAndSize: %08x\n", *(DWORD*)(((ULONG64)pMapImage) + *(LONG*)((ULONG64)pMapImage + 0x3C) + 0x84));
+    fprintf(stdout, "\t\tImageBase: %016llX\n", *(ULONGLONG*)(((ULONG64)pMapImage) + e_lfanew + 0x30));
+    fprintf(stdout, "\t\tSectionAligment: %08X\n", *(DWORD*)(((ULONG64)pMapImage) + e_lfanew + 0x38));
+    fprintf(stdout, "\t\tFileAligment: %08X\n", *(DWORD*)(((ULONG64)pMapImage) + e_lfanew + 0x3C));
+    fprintf(stdout, "\t\tMajorSybsystemVersion: %04X\n", *(WORD*)(((ULONG64)pMapImage) + e_lfanew + 0x48));
+    fprintf(stdout, "\t\tSizeOfImage: %08X\n", *(DWORD*)(((ULONG64)pMapImage) + e_lfanew + 0x50));
+    fprintf(stdout, "\t\tSizeOfHeaders: %08X\n", *(DWORD*)(((ULONG64)pMapImage) + e_lfanew + 0x54));
+    fprintf(stdout, "\t\tSubsystem: %04X\n", *(WORD*)(((ULONG64)pMapImage) + e_lfanew + 0x5C));
+    NumberOfRvaAndSize = (DWORD*)(((ULONG64)pMapImage) + e_lfanew + 0x84);
+    fprintf(stdout, "\t\tNumberOfRvaAndSize: %08X\n", *NumberOfRvaAndSize);
     fprintf(stdout, "\n\t\tIMAGE_DATA_DIRECTORY\n");
 
+    pDataDirectory = (PIMAGE_DATA_DIRECTORY)(((ULONG64)pMapImage) + e_lfanew + 0x88);
+    for (int i = 0; i < *NumberOfRvaAndSize; i++) {
+        fprintf(stdout, "\t\tDirectory %d\n", (i+1));
+        fprintf(stdout, "\t\t RVA:  %08X\n", pDataDirectory[i].VirtualAddress);
+        fprintf(stdout, "\t\t Size: %08X\n", pDataDirectory[i].Size);
+    }
     return 0;
 }
